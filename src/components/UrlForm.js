02c16443d50033a7bbd84b1698120b4df38098da
Lim Ding Han <dinghan@Lims-MacBook-Air-2.local>
2023-03-09 22:31:02 +0800

    fixed axios

--- src/components/UrlForm.js
+++ src/components/UrlForm.js
@@ -1,7 +1,7 @@
 import {useState} from 'react';
+import Axios from 'axios';
 
 const UrlForm = () => {
-
     const [url, setUrl] = useState('');
     const [shortUrl, setShortUrl] = useState('');
     const [displayUrl, setDisplayUrl] = useState(false);
@@ -9,15 +9,20 @@ const UrlForm = () => {
     const handleSubmit = () => {
         
         Axios.post("http://localhost:3001/shortenUrl", {
-            targetUrl: url, 
-            shortUrl,
+            targetUrl: url
         }).then((response) => {
             console.log("URL shortened");
-        });
-        Axios.get("http://localhost:3001/getShortUrl").then((response) => {
-            setUrl(response.data);
+        }).then(() => {
+
+        Axios.get("http://localhost:3001/getShortUrl", {
+            params: {
+                q: url
+            }
+        }).then((response) => {
+            setShortUrl(response.data)
         });
         setDisplayUrl(true);
+    });
     }
 
     
@@ -30,18 +35,18 @@ const UrlForm = () => {
     return (
         <>
         <div>
-            <form>
+            <form onSubmit={e => {e.preventDefault(); handleSubmit();}}>
                 <div className="flex flex-col justify-center items-center min-w-min min-h-min">
                     <label className='lg:text-xl text-lg text-sm'>URL:
                         <input onChange={event => updateUrl(event)} type="text" className=" mx-6 text-black px-2"/>
                     </label>
-                    <input type="button" onClick={handleSubmit} value="Shorten" className="text-sm lg:text-xl text-lg mt-4 bg-black w-24 cursor-pointer"/>
+                    <input type="submit" value="Shorten" className="text-sm lg:text-xl text-lg mt-4 bg-black w-24 cursor-pointer"/>
                 </div>
             </form>
 
             
         </div>
-        <div className='mt-5 text-center lg:text-xl text-lg text-sm'>{displayUrl ? url : ""}</div>
+        <div className='mt-5 text-center lg:text-xl text-lg text-sm'>{displayUrl ? shortUrl : ""}</div>
         </>
     )
 }
