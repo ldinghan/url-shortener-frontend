0802451182aef85c63498375855ad4c39d30d936
Lim Ding Han <dinghan@Lims-MacBook-Air-2.local>
2023-04-03 11:53:57 +0800

    adjusted error text

--- src/components/ErrorMessage.js
+++ src/components/ErrorMessage.js
@@ -3,7 +3,7 @@ const ErrorMessage = () => {
     return (
         <>
             <div className="lg:text-xl text-lg text-sm">
-                This service is currently not available. Head to <a href="https://github.com/ldinghan/url-shortener-frontend">https://github.com/ldinghan/url-shortener-frontend</a> and <a href="https://github.com/ldinghan/url-shortener-backend">https://github.com/ldinghan/url-shortener-backend</a> to check out the source code.
+                This service is currently not available. <br></br>Head to <a className="text-indigo-400" href="https://github.com/ldinghan/url-shortener-frontend">https://github.com/ldinghan/url-shortener-frontend</a><br></br><a className="text-indigo-400" href="https://github.com/ldinghan/url-shortener-backend">https://github.com/ldinghan/url-shortener-backend</a><br></br>to check out the source code.
             </div>
         
         </>
